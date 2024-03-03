ab5fdb9b39948e75d18cb0413bbf2e9233013054
jeaneigsi <128309451+jeaneigsi@users.noreply.github.com>
2024-03-03 11:50:15 +0000

    env

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai import OpenAIEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
 
-st.cache_
 def get_pdf_text(pdf_docs):
     text=""
     for pdf in pdf_docs:
@@ -25,18 +28,38 @@ def get_text_chunks(text):
     chunks=textsplitter.split_text(text)
     return chunks
 
+def get_conversation_chain(vectorestores):
+    llm=ChatGoogleGenerativeAI(model="gemini")
+    memory=ConversationBufferMemory(memory_key='chat history', return_messages=True) # initialize memory
+    conversation_chain=ConversationalRetrievalChain.from_llm(
+        llm=llm,retriever=vectorestores.as_retriever(),memory=memory
+    )
+    return conversation_chain
+
+
 
 def get_vectorestore(text_chunks):
-    embedding=OpenAiEmbeddings()
-    vectorestore=FAISS.from_embeddings(embedding, text_chunks)
+    embedding=OpenAIEmbeddings(model="text-embedding-3-small")
+    vectorestore=FAISS.from_texts(text_chunks,embedding=embedding)
     return vectorestore
 
 
 def main():
     load_dotenv()
     st.set_page_config("Chat with multiple pdfs", page_icon=":books:")
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation=None
+
+
+
+
     st.header("Chat with multiple pdfs :books:")
     st.text_input("Ask a question about your documents: ")
+
+    st.write(user_template.replace("{{MSG}}","Hello robot"), unsafe_allow_html=True)
+    st.write(bot_template.replace("{{MSG}}","Hello human"), unsafe_allow_html=True)
 
     with st.sidebar:
         st.subheader("Your documents")
@@ -52,7 +75,12 @@ def main():
                 # st.write(text_chunks)
 
                 #vetoriser
-                vectorstore=get_vectorstore(text_chunks)
+                vectorstore=get_vectorestore(text_chunks)
+
+                #conversation
+                st.session_state.conversation=get_conversation_chain(vectorstore) #st.state object increase the scope a variable outside st.sidebar
+
+    st.session_state.conversation
 
 
 
